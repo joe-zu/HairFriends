74ddb0be313d7856c8df9c4175ebfe5c8751f3f3
Cory <coryfay97@gmail.com>
2021-02-01 13:26:03 -0800

    adding product_controller and routes

---
+++ controllers/product_controller.js
@@ -0,0 +1,27 @@
+const db = require('../models');
+
+exports.index = (req, res) => {
+    db.Products.findAll({}).then(dbProducts => {
+        res.render(dbProducts)
+    })
+}
+
+exports.delete = (req, res) => {
+    db.Products.destroy({
+        where: {
+            id: req.params.id
+        }
+    }).then(dbDelete => {
+        res.render(dbDelete); 
+    })
+}
+
+exports.create = (req, res) => {
+    db.Products.create({
+        where: {
+            id: req.body.id
+        }
+    }).then(dbCreate => {
+        res.json(dbCreate);
+    })
+}
--- models/products.js
+++ models/products.js
@@ -25,6 +25,6 @@ module.exports = (sequelize, DataTypes) => {
     //       }
     //     });
     //   };
-      return Forms;
+      return Products;
     };
     

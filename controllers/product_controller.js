557f6bbd60a6526849f285bf7dede7455fcb356a
Cory <coryfay97@gmail.com>
2021-02-02 15:18:48 -0800

    pushing in order to pull

--- controllers/product_controller.js
+++ controllers/product_controller.js
@@ -2,9 +2,9 @@ const db = require('../models');
 
 exports.index = (req, res) => {
     db.Products.findAll({}).then(dbProducts => {
-        res.render(dbProducts)
+        res.json(dbProducts)
     })
-}
+    }
 
 exports.delete = (req, res) => {
     db.Products.destroy({
--- routes/product.js
+++ routes/product.js
@@ -3,9 +3,9 @@ var router  = express.Router();
 
 var product_controller = require('../controllers/product_controller');
 
-router.get('/products', product_controller.index);
-router.destroy('/', product_controller.index);
-router.create('/', product_controller.index);
+router.get('/testing', product_controller.index);
+// router.destroy('/', product_controller.index);
+// router.create('/', product_controller.index);
 
 
 module.exports = router;

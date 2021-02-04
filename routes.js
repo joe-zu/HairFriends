e0f5490cc50c399919ad05fc906628ba290ace83
ryry <ryanpetrecca@gmail.com>
2021-02-03 21:18:13 -0800

    added some href to some of the navbar elements to properly display certain pages when htat route is hit

--- routes.js
+++ routes.js
@@ -1,27 +1,14 @@
 module.exports = app => {
 
     const application = require('./routes/application');
-
     const forms = require('./routes/forms');
     const product = require('./routes/product');
-
-<<<<<<< HEAD
-=======
-    
-
->>>>>>> a051455c3ed68b10fa784d60ef80b34752a3d558
     // const trips = require('./routes/trips');
     // const pricing = require('./routes/pricing');
 
     app.use('/', application);
-
     app.use('/forms', forms);
     app.use('/product', product);
-<<<<<<< HEAD
-
-=======
-    
->>>>>>> a051455c3ed68b10fa784d60ef80b34752a3d558
     // app.use('/pricing', pricing);
 //other routes..
 }
--- routes/application.js
+++ routes/application.js
@@ -5,12 +5,6 @@ const application_controller = require('../controllers/application_controller');
 const product_controller = require('../controllers/product_controller');
 router.get('/', application_controller.index);
 
-
-<<<<<<< HEAD
-=======
-
-
->>>>>>> a051455c3ed68b10fa784d60ef80b34752a3d558
 router.get('/products', product_controller.index);
 router.delete('/products/delete/:id', product_controller.delete);
 router.post('/products/create', product_controller.create);

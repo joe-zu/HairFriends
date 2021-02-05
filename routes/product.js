00e86101f84d1deb4ccb99641664bb1490edf552
Cory <coryfay97@gmail.com>
2021-02-04 20:17:05 -0800

    cleaned the code up some

--- routes/product.js
+++ routes/product.js
@@ -4,9 +4,7 @@ var router  = express.Router();
 var product_controller = require('../controllers/product_controller');
 
 router.get('/', product_controller.index);
-//router.get('/', product_controller.products);
 router.delete('/:id', product_controller.delete);
 router.post('/', product_controller.create);
-router.put('/update/:prodName/:prodDesc/:id', product_controller.update)
 
 module.exports = router;

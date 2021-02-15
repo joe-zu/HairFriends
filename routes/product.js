3c6e004283bb74de58b8996c69d6a7725bbd9660
Joe Giampaoli <giampaoli.joe@gmail.com>
2021-02-15 15:10:27 -0800

    product routes auth

--- routes/product.js
+++ routes/product.js
@@ -3,12 +3,14 @@ var router  = express.Router();
 
 var product_controller = require('../controllers/product_controller');
 
+var isAuthenticated = require("../config/middleware/isAuthenticated");
+
 router.get('/', product_controller.index);
 
-router.get('/auth', product_controller.findAll);
+router.get('/auth', isAuthenticated, product_controller.findAll);
 
-router.delete('/auth/delete/:id', product_controller.delete);
+router.delete('/auth/delete/:id', isAuthenticated, product_controller.delete);
 
-router.post('/auth', product_controller.create);
+router.post('/auth', isAuthenticated, product_controller.create);
 
 module.exports = router;
